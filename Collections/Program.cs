c7298e43819face70077ebbcee34078814a7a430
selofuko@outlook.com <selofuko@outlook.com>
2021-01-22 21:46:33 +0300

    20.01.2021 Der Sonu

---
+++ Collections/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string> {"Murat","Kerem","Halil" };
+            isimler.Add("Furkan");
+
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product
+            {
+                Id = 1,
+                CategoryId = 2,
+                ProductName = "Masa",
+                UnitPrice = 500,
+                UnitsInStock = 3
+            };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+
+        }
+    }
+}
